40d4d2416fe4d5eb1b12c8a403fd98763c05f76d
gagachoo <99661996+gagachoo@users.noreply.github.com>
2024-02-05 00:11:51 +0300

    Update маленькая машинка 1.ino

--- маленькая машинка 1.ino
+++ маленькая машинка 1.ino
@@ -1,15 +1,16 @@
 // 1. подключите пины драйвера L298N к цифровым пинам Arduino
- 
+
 // первый двигатель
 int enA = 7;
 int in1 = 6;
 int in2 = 5;
- 
+
 // второй двигатель
 int enB = 2;
 int in3 = 3;
 int in4 = 4;
- 
+
+
 void setup()
 {
   // инициализируем все пины для управления двигателями как outputs
@@ -20,80 +21,50 @@ void setup()
   pinMode(in3, OUTPUT);  
   pinMode(in4, OUTPUT);
 }
- 
-// эта функция обеспечит вращение двигателей в двух направлениях на установленной скорости
-void demoOne()
-{  
-  // запуск двигателя A
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  // устанавливаем скорость 100 из доступного диапазона 0~255
-  analogWrite(enA, 100);
- 
-  // запуск двигателя B
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  // устанавливаем скорость 100 из доступного диапазона 0~255
-  analogWrite(enB, 100);
-  
-  delay(2000);
- 
-  // меняем направление вращения двигателей
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
- 
-  delay(2000);
- 
-  // выключаем двигатели
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
+
+void loop() {
+
+
+    forward();
+    delay(2000); // пауза 2сек
+    stop();
+    delay(3000);
+
+
+    back();
+    delay(2000); // пауза 2сек
+    delay(3000);
 }
- 
-// эта функция обеспечивает работу двигателей во всем диапазоне возможных скоростей
-void demoTwo()
-{
-  // обратите внимание, что максимальная скорость определяется самим двигателем и напряжением питания
-  // ШИМ-значения генерируются функцией analogWrite()
-  // и зависят от вашей платы управления
- 
-  // запускаем двигатели
+
+
+
+void forward(){ 
+    // выставляем 100% мощность на моторе А - 255 из 255
+    analogWrite( enA, 255 );
+    analogWrite( enB, 255 );
+    // выставляем режим мотора - вращение по часовой
+    digitalWrite( in1, HIGH );
+    digitalWrite( in2, LOW );
+    digitalWrite( in3, LOW );
+    digitalWrite( in4, HIGH );
+}
+
+
+void back(){ 
+    // выставляем мощность на мотора А - 150 из 255
+    analogWrite( enA, 255 );
+    analogWrite( enB, 255 );
+    // режим мотора - вращение против часовой
+    digitalWrite( in1, LOW );
+    digitalWrite( in2, HIGH );
+    digitalWrite( in3, HIGH );
+    digitalWrite( in4, LOW );
+}
+
+
+void stop() {
   digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
+  digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
- 
-  // ускорение от нуля до максимального значения
-  for (int i = 0; i < 256; i++)
-  {
-    analogWrite(enA, i);    
-    analogWrite(enB, i);    
-    delay(20);
+  digitalWrite(in4, LOW);
   }
- 
-  // торможение от максимального значения к минимальному
-  for (int i = 255; i >= 0; --i)
-  {
-    analogWrite(enA, i);
-    analogWrite(enB, i);
-    delay(20);
-  }
- 
-  // теперь отключаем моторы
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-}
- 
-void loop()
-{
-  demoOne();
-  delay(1000);
-  
-  demoTwo();
-  delay(1000);
-}
