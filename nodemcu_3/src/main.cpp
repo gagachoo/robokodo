1988169e5de09316b68a8c939f6edbfe4dc56f7a
gagachoo <gagachoo@yandex.ru>
2024-02-14 09:30:48 +0300

    2

--- nodemcu_3/src/main.cpp
+++ nodemcu_3/src/main.cpp
@@ -1,18 +1,13 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
+//#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#define LED D8
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+pinMode(LED,OUTPUT);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(LED, 200);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
 }
